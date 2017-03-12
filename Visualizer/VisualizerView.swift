29ede5287c1853e1de3cf32475424e0846658feb
Albert Smith <albert.chejin.edward.smith@outlook.com>
2017-03-12 16:44:05 +0800

    Add AudioKit Framework

--- Visualizer/VisualizerView.swift
+++ Visualizer/VisualizerView.swift
@@ -9,9 +9,7 @@
 import Cocoa
 import ScreenSaver
 import QuartzCore
-import CoreAudioKit
-import AVFoundation
-import Accelerate
+import AudioKit
 
 // We'll first try a STFT with 0% overlap and a half-cosine window.
 // of course something like 50% overlap is more accurate.
@@ -24,71 +22,33 @@ let outScale = 1.0
 let bufferLength = 100
 
 
-
-// Adapted from https://www.objc.io/issues/16-swift/rapid-prototyping-in-swift-playgrounds/
-func sqrt(_ x: [Double]) -> [Double] {
-    var results = [Double](repeating:0.0, count:x.count)
-    vvsqrt(&results, x, [Int32(x.count)])
-    return results
-}
-
-let fft_weights: FFTSetupD = vDSP_create_fftsetupD(vDSP_Length(log2(Float(bufferLength))), FFTRadix(kFFTRadix2))!
-
-func fft(inputArray:[Double]) -> [Double] {
-    var inputArray = inputArray
-    var fftMagnitudes = [Double](repeating:0.0, count:inputArray.count)
-    var zeroArray = [Double](repeating:0.0, count:inputArray.count)
-    var splitComplexInput = DSPDoubleSplitComplex(realp: &inputArray, imagp: &zeroArray)
-    
-    vDSP_fft_zipD(fft_weights, &splitComplexInput, 1, vDSP_Length(log2(CDouble(inputArray.count))), FFTDirection(FFT_FORWARD));
-    vDSP_zvabsD(&splitComplexInput, 1, &fftMagnitudes, 1, vDSP_Length(inputArray.count));
-    
-    let roots = sqrt(fftMagnitudes) // vDSP_zvmagsD returns squares of the FFT magnitudes, so take the root here
-    var normalizedValues = [Double](repeating:0.0, count:inputArray.count)
-    
-    vDSP_vsmulD(roots, vDSP_Stride(1), [2.0 / Double(inputArray.count)], &normalizedValues, vDSP_Stride(1), vDSP_Length(inputArray.count))
-    return normalizedValues
-}
-
-
-
 class VisualizerView: ScreenSaverView {
     var rects: [NSRect] = []
     let audioEngine = AVAudioEngine()
     
-    var needsFFT = false
+    var input: AKMicrophone! = nil
+    var mixer: AKMixer! = nil
+    var output: AKBooster! = nil
+    var fft: AKFFTTap! = nil
     
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
         
         initRects()
         
-        let inputNode = audioEngine.inputNode!
-        let bus = 0
+        Swift.print(AudioKit.availableInputs!)
         
-        inputNode.installTap(onBus: bus, bufferSize: AVAudioFrameCount(bufferLength), format: inputNode.inputFormat(forBus: bus)) {
-            (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
-                if self.needsFFT {
-                    self.needsFFT = false
-                    var samples: [Double] = []
-                    // Store the samples
-                    for i in 0 ..< buffer.frameLength {
-                        samples.append(Double(buffer.floatChannelData![0][Int(i)]))
-                    }
-                    let heights = fft(inputArray: samples)
-                
-                    for i in 0 ..< numBars {
-                        self.rects[i].size.height = CGFloat(heights[i] * Double(frame.size.height) * 50 * outScale)
-                    }
-                }
-        }
+        input = AKMicrophone()
+        do { try input.setDevice(AudioKit.availableInputs![0]) } catch {}
+        mixer = AKMixer(input)
+        output = AKBooster(mixer, gain:0)
         
-        audioEngine.prepare()
-        do {
-            try audioEngine.start()
-        } catch {
-            Swift.print("Error")
-        }
+        AudioKit.output = output
+        AudioKit.start()
+        
+        output.start()
+        
+        fft = AKFFTTap(mixer)
         
         self.animationTimeInterval = 1.0/30.0
     }
@@ -127,8 +87,10 @@ class VisualizerView: ScreenSaverView {
     }
     
     override func animateOneFrame() {
+        for i in 0..<numBars {
+            rects[i].size.height = CGFloat(fft.fftData[i]*outScale)*self.frame.height
+        }
         self.needsDisplay = true
-        self.needsFFT = true
     }
     
     override func hasConfigureSheet() -> Bool {
